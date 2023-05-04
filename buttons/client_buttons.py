a48ed263575045cb6d18abf281e0d07f79ed6ba0
Alexey Osipov <osipov27032002@mail.ru>
2023-05-04 12:18:02 +0400

    resize buttons

--- buttons/client_buttons.py
+++ buttons/client_buttons.py
@@ -4,7 +4,7 @@ b1 = KeyboardButton('/show')
 b2 = KeyboardButton('/add')
 b3 = KeyboardButton('/feedback')
 
-button_client = ReplyKeyboardMarkup()
+button_client = ReplyKeyboardMarkup(resize_keyboard=True)
 
-button_client.add(b1).add(b2).add(b3)
+button_client.add(b3).add(b2).insert(b1)
 
